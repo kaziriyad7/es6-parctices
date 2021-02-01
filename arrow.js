ac3e72bdb85e7b21cfbdc5a366ca8ba28fc5d24c
kazi riyad <kazimohiuddinriyad77@gmail.com>
2021-02-01 22:15:44 +0600

    Some working ES6 javascript

---
+++ arrow.js
@@ -0,0 +1,27 @@
+// function double(num){
+//     return num * 2;
+// }
+// const result = double(20);
+// console.log(result);
+
+// const doubleIt = function(num){
+//     return num * 2;
+// };
+// const result = doubleIt(10);
+// console.log(result);
+
+
+// const doubleIt = Num => Num * 2;
+
+// const result = doubleIt(10);
+// console.log(result);
+
+
+const bishal = (x,y) =>{
+    const sum = x + y;
+    const diff = x - y;
+    const result = sum * diff;
+    return result;
+}
+const result = bishal(10, 20);
+console.log(result);
---
+++ templete.js
@@ -0,0 +1,14 @@
+const firstName = 'Kazi';
+const middleName = 'Mohiuddin';
+
+const lastName = 'Riyad';
+
+const TotalNamne = `${firstName} ${middleName} ${lastName}`;
+console.log(TotalNamne);
+// 
+
+const multiLine = `jodi tor dhak shune
+kew nah ashe..
+tobe ekla colo ree...`;
+
+console.log(multiLine);
